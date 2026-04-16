ef37746563900b793df727c97fbc02ca25c90d94
medev-fsai <256591220+medev-fsai@users.noreply.github.com>
2026-04-16 22:15:43 +0100

    Fix dynamic grid size input.

--- script.js
+++ script.js
@@ -4,23 +4,35 @@ STEP 1: Sketchpad construction
 STEP 2: Add event listener to each element of sketchpad.
 */
 const MAX_SIZE = 700;
-const SKETCH_SIZE = +prompt("Enter number of squares per row: (1-25)", '16');
-const SQUARE_SIZE = MAX_SIZE/SKETCH_SIZE;
-
+const SKETCH_SIZE = 8;
 const sketchpad = document.querySelector("#sketch-container");
-for(let i=0; i<SKETCH_SIZE**2;){
-    let square = document.createElement("div");
-    square.classList.toggle("item");
-    square.setAttribute("id", `square-${++i}`)
-    square.style.setProperty("width", `${SQUARE_SIZE}px`);
-    square.style.setProperty("height", `${SQUARE_SIZE}px`);
-    sketchpad.appendChild(square);
+const inputSizeButton = document.querySelector("button");
+
+inputSizeButton.addEventListener("click", ()=>{
+    const size = +prompt("Enter grid size : (1 - 100 max)", '16');
+    clearSketchpad();
+    createNewSketchpad(size);
+});
+
+createNewSketchpad(SKETCH_SIZE);
+
+function createNewSketchpad(size){
+    const squareSize = MAX_SIZE / size;
+    for(let i=0; i<size**2;){
+        let square = document.createElement("div");
+        square.classList.toggle("item");
+        square.setAttribute("id", `square-${++i}`)
+        square.style.setProperty("width", `${squareSize}px`);
+        square.style.setProperty("height", `${squareSize}px`);
+        sketchpad.appendChild(square);
+        //Add event listener to square
+        square.addEventListener("mouseover", () => {
+            square.classList.add("dark");
+        });
+    }
 }
 
-let squaresList = document.querySelectorAll(".item");
-squaresList.forEach((item) => {
-    item.addEventListener("mouseover", () => {
-        item.classList.add("dark");
-    });
-});
+function clearSketchpad(){
+    sketchpad.replaceChildren();
+}
 

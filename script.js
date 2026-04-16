928bb0aafdf4a7b9a137658b991e953655ba91f7
medev-fsai <256591220+medev-fsai@users.noreply.github.com>
2026-04-16 21:06:30 +0100

    Fix out of layout problem. Force flex 0 0 auto, and set height and width manually without rounding. It solves the problem, but for some values like 51, still there is out of container problem. width, heigh become dicimal with long after comma values, so rounding do not work properly. It looks like this is the limit for flexbox.

--- script.js
+++ script.js
@@ -3,16 +3,17 @@
 STEP 1: Sketchpad construction
 STEP 2: Add event listener to each element of sketchpad.
 */
-const MAX_SIZE = 480;
-const SKETCH_SIZE = 16//+prompt("Enter number of squares per row: (1-25)", '16');
-const SQUARE_SIZE = Math.floor(MAX_SIZE/SKETCH_SIZE);
+const MAX_SIZE = 700;
+const SKETCH_SIZE = +prompt("Enter number of squares per row: (1-25)", '16');
+const SQUARE_SIZE = MAX_SIZE/SKETCH_SIZE;
 
 const sketchpad = document.querySelector("#sketch-container");
 for(let i=0; i<SKETCH_SIZE**2;){
     let square = document.createElement("div");
     square.classList.toggle("item");
     square.setAttribute("id", `square-${++i}`)
-    square.style.setProperty("min-width", `${SQUARE_SIZE}px`);
+    square.style.setProperty("width", `${SQUARE_SIZE}px`);
+    square.style.setProperty("height", `${SQUARE_SIZE}px`);
     sketchpad.appendChild(square);
 }
 

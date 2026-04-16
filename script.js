f7d21cd3d06961fc7ccad513538b8194b8d966b2
medev-fsai <256591220+medev-fsai@users.noreply.github.com>
2026-04-16 19:35:43 +0100

    Add basic logic to the sketchpad.

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Script working!")
+
+/*
+STEP 1: Sketchpad construction
+STEP 2: Add event listener to each element of sketchpad.
+*/
+const MAX_SIZE = 480;
+const SKETCH_SIZE = 16//+prompt("Enter number of squares per row: (1-25)", '16');
+const SQUARE_SIZE = Math.floor(MAX_SIZE/SKETCH_SIZE);
+
+const sketchpad = document.querySelector("#sketch-container");
+for(let i=0; i<SKETCH_SIZE**2;){
+    let square = document.createElement("div");
+    square.classList.toggle("item");
+    square.setAttribute("id", `square-${++i}`)
+    square.style.setProperty("min-width", `${SQUARE_SIZE}px`);
+    sketchpad.appendChild(square);
+}
+
+let squaresList = document.querySelectorAll(".item");
+squaresList.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+        item.classList.add("dark");
+    });
+});
+
